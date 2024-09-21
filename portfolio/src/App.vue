62241ef1140a7af3670247d47e9260f0b865e607
Pyriac <cyriacpineau@gmail.com>
2024-09-21 16:39:06 +0200

    initialize project

---
+++ portfolio/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <div>
+    <img
+      class="header"
+      src="./assets/Header.jpg"
+      alt="Illustration d'un bureau avec une tasse de café, un ordinateur et une fenêtre qui donne sur des montagnes"
+    />
+  </div>
+  <HelloWorld msg="Bienvenue" />
+</template>
+
+<style scoped></style>
---
+++ portfolio/src/components/HelloWorld.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>BIENVENUE</h1>
+
+  <div class="card">
+    <p>Vous êtes sur mon portfolio, je m'appele Cyriac.</p>
+    <p>J'utilise Vue.js pour la première fois</p>
+  </div>
+</template>
+
+<style scoped></style>
