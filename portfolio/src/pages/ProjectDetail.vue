8d4bf4b2678e4e3b7b25666560d6a3bcd911b702
Pyriac <cyriacpineau@gmail.com>
2024-10-16 19:53:30 +0200

    FEATURE: data displayed il projectDetail

--- portfolio/src/pages/ProjectDetail.vue
+++ portfolio/src/pages/ProjectDetail.vue
@@ -5,8 +5,34 @@ const route = useRoute();
 const project = projectData.find(
   (project) => project.name === route.params.name
 );
+console.info(project);
 </script>
 
 <template>
-  <h1>Coucou de {{ project.name }}</h1>
+  <h1>{{ project.name }}</h1>
+  <p>{{ project.description }}</p>
+  <img :src="`/images/${project.image}`" alt="" />
+  <p>{{ project.technologies }}</p>
+  <div class="link-zone">
+    <a :href="`${project.github}`" target="_blank">
+      <img
+        v-if="project.github"
+        src="/GitHub.png"
+        alt="Liens vers le repository github"
+      />
+    </a>
+    <a :href="`${project.website}`" target="_blank">
+      <img
+        v-if="project.website"
+        src="/website.png"
+        alt="Liens vers le site internet"
+      />
+    </a>
+  </div>
 </template>
+
+<style scoped>
+img {
+  width: 88%;
+}
+</style>
